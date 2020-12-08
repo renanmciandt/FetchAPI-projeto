4fac8d078e45c74351ed520334c2487aba01def1
Renan <renanm@ciandt.com>
2020-12-08 16:11:49 -0300

    Adicionei o Editar na lista de clientes

--- api/cliente.js
+++ api/cliente.js
@@ -8,6 +8,7 @@ const listarClientes = () => {
     })
 }
 
+// Cadastro de clientes
 const cadastrarClientes = (nome, cpf) => {
     const json = JSON.stringify({
         nome: nome,
@@ -25,8 +26,34 @@ const cadastrarClientes = (nome, cpf) => {
     })
 }
 
+// Exclusão de clientes 
 const deletaCliente = id => {
     return fetch(`http://localhost:4000/clientes/cliente/${id}`, {
-        method: 'DELETE',
+        method: 'DELETE'
+    })
+}
+
+// Edição dos Clientes
+const detalhaCliente = id => {
+    return fetch(`http://localhost:4000/clientes/cliente/${id}`,
+    {
+        method: 'GET'
+    })
+    .then(resposta => {
+        return resposta.json()
+    })
+}
+
+const editaCliente = (id, cpf, nome) => {
+    const json = JSON.stringify({
+        nome: nome,
+        cpf: cpf
+    })
+    return fetch(`http://localhost:4000/clientes/cliente/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: json
     })
 }
--- listagem-cliente.js
+++ listagem-cliente.js
@@ -12,6 +12,9 @@ const exibeCliente = (cpf, nome, id) => {
         <td>${cpf}</td>
         <td>${nome}</td>
         <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
+        <a href="edita-clientes.html?id=${id}">
+            <button type="button" class="btn btn-info">Editar</button>
+        </a>
 `;
     linha.innerHTML = conteudoLinha;
     return linha;
