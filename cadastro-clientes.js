f4c38f02fa20b4a97bd61322ce129e01bc706ebf
Renan <renanm@ciandt.com>
2020-12-08 15:06:35 -0300

    Validação do CPF

--- cadastro-clientes.js
+++ cadastro-clientes.js
@@ -3,8 +3,12 @@ const formCadstroCliente = document.querySelector('[data-form]')
 formCadstroCliente.addEventListener("submit", event => {
     event.preventDefault()
     
-    const nome = event.target.querySelector('[data-nome]')
-    const cpf = event.target.querySelector('[data-cpf]')
+    const nome = event.target.querySelector('[data-nome]').value
+    const cpf = event.target.querySelector('[data-cpf]').value
 
-    cadastrarClientes(nome.value, cpf.value)
+    if(validaCPF(cpf)) {
+        cadastrarClientes(nome, cpf)
+    } else {
+        alert('O CPF não é válido!')
+    }
 })
